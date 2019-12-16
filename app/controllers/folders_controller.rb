2bf2c82c177032e6a337add9657e7128b11d75da
hayamizhin <max9173627>
2019-12-16 17:10:49 +0900

    [add]folder/newのview画面を作成

--- app/controllers/folders_controller.rb
+++ app/controllers/folders_controller.rb
@@ -1,8 +1,14 @@
 class FoldersController < ApplicationController
   def index
+    @folder = Folder.all
   end
 
   def new
+    @folder = Folder.new
+  end
+
+  def create
+    @folder = Folder.new(folder_params)
   end
 
   def edit
@@ -10,4 +16,11 @@ class FoldersController < ApplicationController
 
   def show
   end
+
+  private
+
+    def folder_params
+      params.reqire(:folder).permit(:folder_name)
+    end
+
 end
