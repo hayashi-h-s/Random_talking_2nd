1e68793e30413982b362aad81da4241e8fbeedaf
hayamizhin <max9173627>
2020-01-22 11:25:14 +0900

    [change]postgreSQLからMySQLへ変更

--- db/schema.rb
+++ db/schema.rb
@@ -12,16 +12,13 @@
 
 ActiveRecord::Schema.define(version: 2019_12_20_073118) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
-  create_table "folders", force: :cascade do |t|
+  create_table "folders", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "title"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table "posts", force: :cascade do |t|
+  create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.text "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
