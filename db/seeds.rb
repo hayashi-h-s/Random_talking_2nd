1953b6008470ff11f1bbeba8f17757ff02c92791
hayamizhin <max9173627>
2020-01-23 22:23:00 +0900

    [add]seedデータの更新

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,9 @@
-@folder = Folder.create(title:  "seed_samole")
+@folder1 = Folder.create(title:  "seed_samole1")
 
-@posts = @folder.posts.create(content: "シードサンプル(posts)")
+@posts1 = @folder1.posts.create(content: "シードサンプル(post1)1"),
+          @folder1.posts.create(content: "シードサンプル(post2)1")
+
+@folder2 = Folder.create(title:  "seed_samole2")
+
+@posts2 = @folder2.posts.create(content: "シードサンプル(post1)2"),
+          @folder2.posts.create(content: "シードサンプル(post2)2")
