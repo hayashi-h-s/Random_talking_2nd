a3e6c479abca77119d82e80ffe5d196d104b4dae
hayamizhin <max9173627>
2020-01-26 14:24:41 +0900

    [add]folders/showにpostscのcountを表示する

--- app/controllers/folders_controller.rb
+++ app/controllers/folders_controller.rb
@@ -26,6 +26,7 @@ class FoldersController < ApplicationController
   def show
     @folder = Folder.find(params[:id])
     @posts = @folder.posts.all
+    @posts_count = @posts.size
     @random = @folder.posts.order("RAND()").limit(1)
   end
 
