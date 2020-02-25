dd8f10380ed7961781eb8bed47b9d550d1824ad7
hayamizhin <max9173627>
2020-02-25 12:29:27 +0900

    [remove]user_modelの削除

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_18_075801) do
+ActiveRecord::Schema.define(version: 2020_02_25_032728) do
 
   create_table "folders", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "title"
@@ -26,14 +26,6 @@ ActiveRecord::Schema.define(version: 2020_02_18_075801) do
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "folder_id"
     t.index ["folder_id"], name: "index_posts_on_folder_id"
-  end
-
-  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
-    t.string "name"
-    t.string "email"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.string "password_digest"
   end
 
 end
